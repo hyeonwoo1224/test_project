d28ccd4bd59a54563c5278f137b435fbea18e68c
jungyooseok <wjddbtjr8888@naver.com>
2021-08-16 11:57:40 +0900

    확인

--- src/test_project_jungyooseok/spicy_junhyooseok.java
+++ src/test_project_jungyooseok/spicy_junhyooseok.java
@@ -1,5 +1,7 @@
 package test_project_jungyooseok;
 
+import java.util.Scanner;
+
 public class spicy_junhyooseok {
     public int spicy;
     
@@ -9,4 +11,16 @@ public class spicy_junhyooseok {
     public void spicyDown() {
        --spicy;
     }
+    Scanner input = new Scanner(System.in);
+    
+    int ch = input.nextInt()
+	if(ch == 1) {
+		t.intensityUp();
+	}else if(ch == 2 && t.intensity>0)  {
+		t.intensityDown();
+	}
+	System.out.println("���� ǳ�� : "+t.intensity);
+}else {
+	System.out.println("������ ���� Ű����");
+    
 }
