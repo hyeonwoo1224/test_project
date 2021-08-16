05b9e784b9868f2a8caacfe88e763826f55e7157
seohee <zawadi0805@gmail.com>
2021-08-16 12:12:49 +0900

    메서드 계산기능 추가

--- src/test_project_seohee/count_seohee.java
+++ src/test_project_seohee/count_seohee.java
@@ -5,19 +5,26 @@ public class count_seohee {
 
 	public int jajangCnt = 0;
 	public int jjamppongCnt = 0;
-	
+	int jajang =0, jjamppon=0,sum=jajang+jjamppon;
 	
 	public void jajangCntUp() {
 		jajangCnt++;
+		jajang+= 4000;
+		sum = jajang+ jjamppon;
 	}
 	public void jajangCntDown() {
 		jajangCnt--;
+		jajang-= 4000;
+		sum = jajang+ jjamppon;
 	}
 	public void jjamppongCntUP() {
 		jjamppongCnt++;
+		jjamppon+=6000;
+		sum = jajang+jjamppon;
 	}
 	public void jjamppongCntDown() {
 		jjamppongCnt--;
-		
+		jjamppon-=6000;
+		sum = jajang+jjamppon;
 	}
 }
