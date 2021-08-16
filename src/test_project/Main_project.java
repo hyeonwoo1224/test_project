615323191cbca2ddaaaf2442b5b1501ce12f7697
hyeonwoo <ejffoddl1@naver.com>
2021-08-16 13:11:25 +0900

    내려받기 후 한글 깨짐 수정

--- src/test_project/Main_project.java
+++ src/test_project/Main_project.java
@@ -25,10 +25,10 @@ public class Main_project {
 
 		int num;
 		while(true) {
-		System.out.println("1.�޴� �� ���� ����");
-		System.out.println("2.�ҽ� ����");
-		System.out.println("3.�ʱ� ����");
-		System.out.println("4.�ݾ� Ȯ��");
+		System.out.println("1.메뉴 및 수량 선택");
+		System.out.println("2.소스 선택");
+		System.out.println("3.맵기 선택");
+		System.out.println("4.금액 확인");
 		System.out.println(">>>>>>");
 
 
@@ -38,64 +38,62 @@ public class Main_project {
 		
 		switch(num) {
 		case 1 :
-			System.out.println("1.¥��� 2.«�� ");
+			System.out.println("1.짜장면 2.짬뽕 ");
 			System.out.println(">>>>>>");
 
 			num= input.nextInt();
 			
 			if(num==1) {
-				System.out.println("1.¥��� �߰�  2. ¥��� ���");
+				System.out.println("1.짜장면 추가  2. 짜장면 취소");
 				 int ja = input.nextInt();
 				if(ja == 1) {
 					cnt.jajangCntUp();
-					System.out.println("¥��� ����: " + cnt.jajangCnt);
-					System.out.println("«�� ����: " + cnt.jjamppongCnt);
+					System.out.println("짜장면 갯수: " + cnt.jajangCnt);
+					System.out.println("짬뽕 개수: " + cnt.jjamppongCnt);
 				} else if(ja == 2 && cnt.jajangCnt>=0){
 					cnt.jajangCntDown();
-					System.out.println("¥��� ����: " + cnt.jajangCnt);
-					System.out.println("«�� ����: " + cnt.jjamppongCnt);
+					 System.out.println("짜장면 갯수: " + cnt.jajangCnt);
+		               System.out.println("짬뽕 갯수: " + cnt.jjamppongCnt);
 				}
 			}else {
-				System.out.println("1. «�� �߰�  2. «�� ���");
+				System.out.println("1. 짬뽕 추가  2. 짬뽕 취소");
 				int jjam = input.nextInt();
-				if(jjam == 1) {
-					cnt.jjamppongCntUP();
-					System.out.println("«�� ����: " + cnt.jjamppongCnt);
-					System.out.println("¥��� ����: " + cnt.jajangCnt);
-				} else if(jjam == 2 && cnt.jjamppongCnt>=0) {
-					cnt.jjamppongCntDown();
-					System.out.println("«�� ����: " + cnt.jjamppongCnt);
-					System.out.println("¥��� ����: " + cnt.jajangCnt);
-				} else {System.out.println("��ȣ�� Ȯ�����ּ���.");}
-			}
-			hy.jajanInput();
+	            if(jjam == 1) {
+	               cnt.jjamppongCntUP();
+	               System.out.println("짬뽕 갯수: " + cnt.jjamppongCnt);
+	               System.out.println("짜장면 갯수: " + cnt.jajangCnt);
+	            } else if(jjam == 2 && cnt.jjamppongCnt>=0) {
+	               cnt.jjamppongCntDown();
+	               System.out.println("짬뽕 갯수: " + cnt.jjamppongCnt);
+	               System.out.println("짜장면 갯수: " + cnt.jajangCnt);
+	            } else {System.out.println("번호를 확인해주세요.");}
+	         }
+
+	         hy.jajanInput();
 
 
-			hy.jajanInput();
-
-
-			break;
+	         break;
 
 
 
-		case 2 : System.out.println("�ҽ� �� ����");
-			break;
-			
-			
-		case 3 :
-			System.out.println("1. �ʱ����� UP, 2. �ʱ����� Down");
-			break;
-			
-		case 4 :
-			System.out.println("�ֹ��� Ȯ���մϴ�.");
-			System.out.println("¥��� : ");
-			System.out.println("«�� : ");
-			System.out.println("�� �ݾ� : ");
-			break;
-		
-		default : 
-			System.out.println("�ٽ� �����ϼ���"); 
-		}
+	      case 2 : System.out.println("소스 양 선택");
+	         break;
+	         
+	         
+	      case 3 :
+	         System.out.println("1. 맵기정도 UP, 2. 맵기정도 Down");
+	         break;
+	         
+	      case 4 :
+	         System.out.println("주문을 확인합니다.");
+	         System.out.println("짜장면 : ");
+	         System.out.println("짬뽕 : ");
+	         System.out.println("총 금액 : ");
+	         break;
+	      
+	      default : 
+	         System.out.println("다시 선택하세요"); 
+	      }
+	   }
 	}
-}
-}
+	}
